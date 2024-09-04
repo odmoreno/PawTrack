a002845aec24bde922aacd23aabbff80a6f53772
Oscar Moreno <oscar.moreno@espol.edu.ec>
2024-09-04 02:27:49 -0500

    errors form

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -51,7 +51,13 @@ export default function PatientForm() {
             className="w-full p-3  border border-gray-100"
             type="text"
             placeholder="Nombre del Propietario"
+            {...register('caretaker', {
+              required: 'El Porpietario es obligatorio'
+            })}
           />
+          {errors.caretaker && (
+            <Error>{errors.caretaker?.message?.toString()}</Error>
+          )}
         </div>
 
         <div className="mb-5">
@@ -63,7 +69,17 @@ export default function PatientForm() {
             className="w-full p-3  border border-gray-100"
             type="email"
             placeholder="Email de Registro"
+            {...register("email", {
+              required: "El Email es Obligatorio",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'Email No Válido'
+              }
+            })}
           />
+          {errors.email && (
+            <Error>{errors.email?.message?.toString()}</Error>
+          )}
         </div>
 
         <div className="mb-5">
@@ -74,7 +90,13 @@ export default function PatientForm() {
             id="date"
             className="w-full p-3  border border-gray-100"
             type="date"
+            {...register('date', {
+              required: 'La fecha de alta es obligatorio'
+            })}
           />
+          {errors.date && (
+            <Error>{errors.date?.message?.toString()}</Error>
+          )}
         </div>
 
         <div className="mb-5">
@@ -85,7 +107,13 @@ export default function PatientForm() {
             id="symptoms"
             className="w-full p-3  border border-gray-100"
             placeholder="Síntomas del paciente"
-          ></textarea>
+            {...register('symptoms', {
+              required: 'Los sintomas son obligatorios'
+            })}
+          />
+          {errors.symptoms && (
+            <Error>{errors.symptoms?.message?.toString()}</Error>
+          )}
         </div>
 
         <input
